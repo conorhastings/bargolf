1c6e1e7511e42078ddaa6326f749aee743635c62
conorhastings <hastings.conorm@gmail.com>
2014-11-01 11:24:41 -0400

    bootstrap

--- schema.sql
+++ schema.sql
@@ -4,14 +4,11 @@ CREATE TABLE teams(
  password varchar
 );
 
-CREATE TABLE team_members(
- id serial primary key,
- name text,
- team_id integer
-);
+
 
 CREATE TABLE bars(
  id serial primary key,
  team_id integer,
- score integer
+ score integer,
+ name text
  );
